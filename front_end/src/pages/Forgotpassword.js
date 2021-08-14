c1c2be5737ca9e89628d3538d3f2ac7d8724aa77
Abdullah Shehu <55896361+Abdul-Asa@users.noreply.github.com>
2021-08-14 02:27:00 +0100

    Update Forgotpassword.js

--- front_end/src/pages/Forgotpassword.js
+++ front_end/src/pages/Forgotpassword.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import Forgotpassword from '../components/EntryPage/forgotpassword'
+import React from "react";
+import Forgotpassword from "../components/EntryPage/forgotpassword";
 // import { Link } from 'react-router-dom'
 
-
 const Forgotpage = () => {
-    return (
-        <div>
-            <Forgotpassword />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Forgotpassword />
+    </div>
+  );
+};
 
-export default Forgotpage
+export default Forgotpage;
